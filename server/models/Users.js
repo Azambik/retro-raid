d48d175f535d79579407a9dbaae7003ef062771c
Azambik <71612017+Azambik@users.noreply.github.com>
2021-04-06 11:09:58 -0400

    pushing user and product models for db.

---
+++ server/models/Users.js
@@ -0,0 +1,54 @@
+const mongoose = require ('mongoose');
+const bcrypt = require('bcrypt');
+const { Schema } = mongoose;
+
+
+
+const userSchema = new Schema({ 
+    firstName: {
+        type: String,
+        require: true,
+        trim: true
+    },
+    lastName: {
+        type: String,
+        Required: true,
+        trim: true
+    },
+    userName: {
+        type: String,
+        Required: true,
+        trim: true,
+        unique:true
+    },
+    email: {
+        type: String,
+        Required: true,
+        unique: true 
+    },
+    password: {
+        type: String,
+        Required: true,
+        minlength:5
+    },
+    //place post topic and order schema here once created. 
+})
+
+// set up pre-save middleware to create password
+userSchema.pre('save', async function(next) {
+    if (this.isNew || this.isModified('password')) {
+      const saltRounds = 10;
+      this.password = await bcrypt.hash(this.password, saltRounds);
+    }
+  
+    next();
+  });
+
+  // compare the incoming password with the hashed password
+userSchema.methods.isCorrectPassword = async function(password) {
+    return await bcrypt.compare(password, this.password);
+  };
+
+  const User = mongoose.model('User', userSchema);
+  
+  module.exports = User
